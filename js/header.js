0e0066bfaee5076997f6204fa07aa16368c6e3a8
Sk7867 <siddhesh.kamble7867@gmail.com>
2024-12-23 16:17:36 +0530

    -Worked on mobile view CSS fixes and hamburger close when clicked on nav menu links

--- js/header.js
+++ js/header.js
@@ -3,6 +3,7 @@ const hamBurger = document.querySelector('.menu_btn_burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu_nav');
 const navItem = document.querySelectorAll('.menu_nav_item');
+const NavMenuItemsAnchorList = document.querySelectorAll('#nav .menu_nav .menu_nav_item a')
 
 let showMenu = false;
 let isAnimating = false
@@ -34,4 +35,36 @@ function toggleMenu() {
   setTimeout(() => {
     isAnimating = false;
   }, 1200);  // Duration slightly longer than the animation (0.3s)
+}
+
+//Add Event listener on all nav anchor links
+NavMenuItemsAnchorList.forEach((anchoreLink) => {
+  anchoreLink.addEventListener('click', handleSectionScroll)
+})
+
+//Check if user is in mobile view, if yes then call userScrollEnded function to close hamburger menu
+function handleSectionScroll(){
+  if(isMobileView(768)){
+    // console.debug('Section scroll active in mobile');
+  toggleMenu()
+    // document.addEventListener('scrollend', userScrollEnded)
+  }
+}
+
+//Function to check if browsers innerWidth is less than given value(e.g. 768) and return true/false
+function isMobileView(value){
+  let browserWidth = window.innerWidth
+  if(browserWidth < value){
+    return true
+  }else {
+    return false
+  }
+}
+
+//Function called after scrollend event to close hamburger menu
+//Not in use as current scroll and hamburger close experience feels better
+function userScrollEnded(){
+  console.debug(typeof(window.onscrollend) !== undefined);
+  // toggleMenu()
+  document.removeEventListener('scrollend', userScrollEnded);
 }
